45816206d12cf3c5b8c970c857a07eb54fd25b04
zhjx922 <zhjx922@foxmail.com>
2016-09-22 18:35:28 +0800

    更新测试脚本

--- memcache.php
+++ memcache.php
@@ -9,23 +9,30 @@ class Test {
 
     public function set() {
         $i = 0;
-        while($i++ < 100000) {
+        while($i++ < 1000) {
             var_dump($this->cache->set("k-{$i}", "v-{$i}", 0));
         }
     }
 
     public function get() {
         $i = 0;
-        while($i++ < 100000) {
+        while($i++ < 1000) {
             var_dump($this->cache->get("k-{$i}"));
         }
     }
 
     public function delete() {
         $i = 0;
-        while($i++ < 100000) {
+        while($i++ < 1000) {
             var_dump($this->cache->delete("k-{$i}"));
         }
+    }
+
+    public function expire() {
+        $this->cache->set("expire", time(), 10);
+        var_dump($this->cache->get("expire"));
+        sleep(11);
+        var_dump($this->cache->get("expire"));
     }
 }
 
@@ -34,3 +41,4 @@ $test->set();
 $test->get();
 $test->delete();
 $test->get();
+$test->expire();
